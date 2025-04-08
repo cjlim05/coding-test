4872ecf58f45b343ac5175e38d4c66c576a858e5
chaeju <85000764+cjlim05@users.noreply.github.com>
2025-04-08 14:29:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.72 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 39 MB, 시간: 68.17 ms
+메모리: 25.4 MB, 시간: 75.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 05일 23:43:51
+2025년 04월 08일 14:29:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,15 @@
 from collections import Counter
 
 def solution(participant, completion):
-    answer = Counter(participant) - Counter(completion)
-    return list(answer.keys())[0]
+    # answer = Counter(participant) - Counter(completion)
+    # return list(answer.keys())[0]
+    participant.sort()
+    completion.sort()
+    
+    # 일치하지 않는 이름을 찾기
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    
+    # 끝까지 일치하면 마지막 참가자가 남은 사람
+    return participant[-1]
